32bdc1584b530cf06027b45cad03bbe4b42f4e46
Faraz Alam <farazalam706@gmail.com>
2022-02-07 11:35:39 +0500

    practise

---
+++ app.js
@@ -0,0 +1,27 @@
+
+// -----------Rest Operator--------
+
+// function sum(name,...arguments) {
+//   sum = 0;
+//   for(let i in  arguments){
+//       sum+=arguments[i];
+//     }
+//     console.log(sum);
+// }
+// sum("Faraz", 20, 30, 40, 90);
+
+// -----------Spread Operator-----------
+
+// function sum(name, ...args) {
+//   mult = 0;
+//   for (let i in args) {
+//       mult+=args[i];
+//   }
+//   console.log(mult);
+// }
+// var arr=[1,2,3,4,5]
+// sum("Faraz",...arr)
+
+// var arr = [1, 2, 3, 4];
+// console.log(arr);
+// console.log([...arr]);
