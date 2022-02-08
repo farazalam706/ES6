d83cf93f0902b00183591f9ee41c38290ffe466b
farazalam706 <farazalam706@gmail.com>
2022-02-08 12:54:27 +0500

    Add files via upload

---
+++ arrowFunction.js
@@ -0,0 +1,16 @@
+function abc() {
+  console.log("Hello");
+}
+abc();
+
+// --------OR--------
+
+let xyz = function () {
+  console.log("World");
+};
+xyz();
+
+// --------OR--------
+
+edf = () => console.log("Faraz");
+edf();
---
+++ strictmode.js
@@ -0,0 +1,10 @@
+
+
+// "use strict"
+
+function myFunc(x,x) {
+  init = 0;
+  sum = init + x;
+  console.log(sum);
+}
+myFunc(20, 30);
