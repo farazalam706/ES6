73db0c5b3ff7fadd04822d96ac658f7b3a9b5716
farazalam706 <farazalam706@gmail.com>
2022-02-08 16:56:27 +0500

    Add files via upload

---
+++ map_filter_findIndex.js
@@ -0,0 +1,64 @@
+// // ---------Map--------
+
+// // var arr = [1, 2, 3, 4];
+// // var arr2 = arr.map((item, index, array) => {
+// // //   return item * 2;
+// // //   return index * 2;
+// //   return array;
+// // });
+// // console.log(arr2);
+
+// // let arr = [
+// //   {
+// //     name: "ali",
+// //     address: "karachi",
+// //     key: 1,
+// //   },
+// //   {
+// //     name: "ahmed",
+// //     address: "lahore",
+// //     key: 2,
+// //   },
+// //   {
+// //     name: "arsalan",
+// //     address: "karachi",
+// //     key: 3,
+// //   },
+// // ];
+// // let arr2 = arr.map((item)=>{
+// //     return item.name;
+// // })
+// // console.log(arr2);
+
+// // ---------Filter--------
+
+// let arr = [
+//   {
+//     name: "ali",
+//     address: "karachi",
+//     key: 1,
+//   },
+//   {
+//     name: "ahmed",
+//     address: "lahore",
+//     key: 2,
+//   },
+//   {
+//     name: "arsalan",
+//     address: "karachi",
+//     key: 3,
+//   },
+// ];
+
+// // let arr2 = arr.filter((item) => {
+// //   return item.name === "ali";
+// // });
+
+// // console.log(arr2);
+
+// // ---------FindIndex--------
+
+// let arr2=arr.findIndex((item)=>{
+//     return  item.name==="ahmed";
+// }) 
+// console.log(arr2);
